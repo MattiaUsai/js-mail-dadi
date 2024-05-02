a22ca5f7fe29831f13693ffca38dfe035a705058
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-02 18:10:06 +0200

    finish html

---
+++ gioco dei dadi/main.js
@@ -0,0 +1,13 @@
+// --al cliccare del bottonָָe gioca il computer dovra[
+//     --generare un numero random per il computer da uno a sei
+//     e scriverlo in pagina
+//     --generare un numero random per il giocatore da uno a sei
+//     e scriverlo in pagina
+//     --se il numero del computer è maggiore del numero giocatore vince il computer,
+//     scrivere in paginà hai perso
+//     altrimenti se il numero del giocatore e piu grande di quello del computer vince il giocatore,
+//     scrivere in pagina hai vinto 
+//     altrimenti se il numero del giocatore ָquello del computer sono uguali uscira parita.
+//     scrivere in pagina parità
+//     ]
+    
